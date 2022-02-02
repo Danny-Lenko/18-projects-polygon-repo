fa7da16ec848a366f74176eac281fd6f38f17671
DannyLenk <danylenko.14@gmail.com>
2022-02-02 14:17:17 +0200

    implemented hex page functionality

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,33 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+"use strict"
+
+window.onload = () => {
+   document.querySelector('#btn').addEventListener('click', controller.manageClick);
+}
+
+let view = {
+
+   setBackground(color) {
+      document.getElementsByTagName('main')[0].style['background-color'] = color;
+   },
+
+   setColorName(color) {
+      document.querySelector('.color').innerHTML = color;
+   }
+
+};
+
+let controller = {
+   hex: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"],
+
+   manageClick() {
+      let hexValues = ['#'];
+
+      for (let i = 0; i < 6; i++) {
+         hexValues.push(controller.hex[Math.floor(Math.random() * 15)])
+      }
+      hexValues = hexValues.join('');
+      view.setBackground(hexValues);
+      view.setColorName(hexValues);
+   },
+
+};
