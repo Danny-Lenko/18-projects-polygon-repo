89dabcd25db3ed06f457896eb6a64052b360b6ad
DannyLenk <danylenko.14@gmail.com>
2022-02-04 04:56:20 +0200

    solved challenge 7

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,16 @@
-//using selectors inside the element
-// traversing the dom
+"use strict"
+
+document.querySelectorAll('.question-btn').forEach((btn, index) => {
+   btn.addEventListener('click', (e) => {
+      const articles = document.querySelectorAll('.question');
+      // const minusIcons = document.querySelectorAll('.minus-icon')
+
+      articles.forEach(article => {
+         if (article !== articles[index]) {
+            article.classList.remove('show-text');
+         }
+      })
+
+      articles[index].classList.toggle('show-text');
+   })
+})
