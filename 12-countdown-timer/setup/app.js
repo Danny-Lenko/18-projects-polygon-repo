5eb00ac4c0ec1e23967fcfa67e572e952bc39c9e
DannyLenk <danylenko.14@gmail.com>
2022-02-07 06:03:48 +0200

    set up basics

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,44 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const todayDate = new Date();
+const todayDayOfMonth = todayDate.getDate();
+const todayMonth = todayDate.getMonth();
+const todayYear = todayDate.getFullYear();
+
+let nextMonth = false;
+let nextYear = false;
+
+const findDaysInMonth = (month, year) => new Date(year, month, 0).getDate();
+const totalDays = findDaysInMonth(todayMonth + 1, todayYear);
+
+const getGiveawayDay = (today, days) => {
+  if (today + 10 <= days) {
+    return today + 10;
+  } else {
+    nextMonth = true;
+    if (todayMonth == 11) {
+      nextYear = true;
+    }
+    return today + 10 - days;
+  }
+}
+
+/* ------------
+Functionality to set giveaway date;
+-------------- */ 
+
+const giveDayOfMonth = getGiveawayDay(todayDayOfMonth, totalDays);
+
+let giveWeekday = 'Monday';
+let giveMonth = 'January';
+let giveDate = 1;
+let giveYear = 2022;
+
+let giveawayFullDate = new Date ()
+
+// const getGiveawayFullDate = () => {
+//   if 
+// }
+
--- README.md
+++ README.md
@@ -145,6 +145,7 @@ document.querySelector('.nav-toggle').addEventListener('click', () => {
 ### Useful resources
 
 - [pageYOffSet](https://stackoverflow.com/a/28144651/16906724) - The answer helped me track the scroll event.
+- [month-days-number](https://stackoverflow.com/a/1184359/16906724) - how to find the number of days in a month.
 
 - [Git + GitHub](https://www.youtube.com/watch?v=RGOj5yH7evk) - the basic git commands (Youtube).
 - [The Markdown Guide](https://www.markdownguide.org/) - for more help with writing markdown (Article).
