bc8233021c9a72fb9f12c1d4d1010de703baa9d4
DannyLenk <danylenko.14@gmail.com>
2022-02-08 06:13:51 +0200

    added basics of alert

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -1,10 +1,27 @@
 // ****** SELECT ITEMS **********
-
+   const alertEl = document.querySelector('.alert');
 // edit option
 
 // ****** EVENT LISTENERS **********
+document.querySelector('.submit-btn').addEventListener('click', () => {
+   const userInput = document.querySelector('#grocery').value;
+   if (!userInput) {
+      alertEl.innerHTML = "Please Enter Value";
+      alertEl.classList.add('alert-danger');
+      const timeoutHide = setTimeout(hideAlert, 1000);
+   }
+
+
+   console.log('hello');
+})
 
 // ****** FUNCTIONS **********
+
+const hideAlert = () => {
+   alertEl.classList.remove('alert-danger');
+   alertEl.innerHTML = "";
+}
+// const timeoutHide = setTimeout(hideAlert, 2000);
 
 // ****** LOCAL STORAGE **********
 
